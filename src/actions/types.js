b4bcede1056b29f8d73f2ac066b1b01bef5f2e1b
Christian Brotherson <christian.brotherson@gmail.com>
2018-05-24 16:22:08 -0600

    finished gallery component

---
+++ src/actions/types.js
@@ -0,0 +1,1 @@
+export const FETCH_CAKES = 'FETCH-CAKES';
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import { connect, dispatch } from 'react-redux';
+import { fetchCake } from '../actions';
 
 class Gallery extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.renderCake = this.renderCake.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.fetchCake()
+    }
+
+    renderCake(cake) {
+        return (
+            <div key={cake.title} className="cakes-container">
+                <div className="cakes">
+                    <img src={cake.img}/>
+                    {/* <img src="http://via.placeholder.com/350x150"/> */}
+                    <div>Title: {cake.title}</div>
+                    <div>Description: {cake.description}</div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div>
-                Gallery
+                {this.props.cakes.map(this.renderCake)}
             </div>
-        );
+        )
     }
 }
 
-export default Gallery;
+function mapStateToProps(state) {
+    return { cakes: state.cakes }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchCake: () => {
+            dispatch(fetchCake())
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gallery);
